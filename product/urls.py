e738e1339e0fabd716f9cc92226ebbc9ebe0b32e
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-04-17 18:50:12 +0530

    added category view serializer url

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,6 @@ from product import views
 
 urlpatterns=[
     path('latest-products/', views.LatestProductList.as_view()),
-    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view())
+    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+    path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Product, Category
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, CategorySerializer
 
 class LatestProductList(APIView):
     def get(self, request,):
@@ -23,4 +23,16 @@ class ProductDetail(APIView):
     def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object( category_slug, product_slug)
         serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
