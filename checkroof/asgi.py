2c314727f5eec00232481caaaf6c3b9b946d04c6
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-04-19 18:22:36 +0530

    checkroof project name

--- checkroof/asgi.py
+++ checkroof/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djackets_django project.
+ASGI config for checkroof project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djackets_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'checkroof.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djackets_django.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'checkroof.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
