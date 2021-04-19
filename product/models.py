dfc6c71124ca04088aa420822949052157eb34cf
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-04-19 18:32:02 +0530

    removing print from product/models.py

--- product/models.py
+++ product/models.py
@@ -17,42 +17,41 @@ class Category(models.Model):
         return f'/{self.slug}/'
 
 class Product(models.Model):
-    category= models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to = 'uploads/', blank = True, null=True)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('-date_added', )
-
+        ordering = ('-date_added',)
+    
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return f'/{self.category.slug}/{self.slug}/'
-
+    
     def get_image(self):
         if self.image:
             return 'http://127.0.0.1:8000' + self.image.url
-        else:
-            return ''
+        return ''
     
     def get_thumbnail(self):
-        print(self)
-        print(self.thumbnail)
         if self.thumbnail:
             return 'http://127.0.0.1:8000' + self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
+
                 return 'http://127.0.0.1:8000' + self.thumbnail.url
             else:
                 return ''
+    
     def make_thumbnail(self, image, size=(300, 200)):
         img = Image.open(image)
         img.convert('RGB')
